b20ab1180d9fb314899c3524bb455432d800b616
Naohide Sano <namachapanda2005@gmail.com>
2026-09-23 12:05:39 +0900

    [mfi] fix doubled code about ucs (by opus5)

--- src/main/java/vavi/sound/mfi/fuetrek/UcsFunction.java
+++ src/main/java/vavi/sound/mfi/fuetrek/UcsFunction.java
@@ -8,6 +8,7 @@ package vavi.sound.mfi.fuetrek;
 
 import javax.sound.midi.Receiver;
 
+import vavi.sound.fuetrek.UcsWaveBank;
 import vavi.sound.mfi.InvalidMfiDataException;
 import vavi.sound.mfi.vavi.sequencer.MachineDependentFunction;
 
@@ -15,9 +16,16 @@ import vavi.sound.mfi.vavi.sequencer.MachineDependentFunction;
 /**
  * A UCS machine dependent function, the same whichever vendor of a fuetrek
  * sound source sends it.
+ * <p>
+ * vavi-sound has classes of its own for the same messages
+ * ({@code vavi.sound.mfi.vavi.sharp.Function16} ~ {@code 18}, {@code panasonic} too), which
+ * only read them. So these are not named after them - a class of the same name in two jars is
+ * whichever the class path has first - and they are of a higher {@link #getPriority() priority}:
+ * they are what puts the waves into the sound source ({@link UcsWaveBank}).
  *
  * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (nsano)
  * @version 0.00 2026-09-15 nsano initial version <br>
+ *          0.01 2026-09-23 nsano the providers, out of vavi-sound's packages <br>
  */
 public abstract class UcsFunction implements MachineDependentFunction {
 
@@ -46,6 +54,12 @@ public abstract class UcsFunction implements MachineDependentFunction {
         return vendor + "." + function;
     }
 
+    /** over vavi-sound's, which only reads the messages */
+    @Override
+    public int getPriority() {
+        return 1;
+    }
+
     @Override
     public void process(byte[] data, Receiver receiver) throws InvalidMfiDataException {
         if (data.length < 7) {
@@ -57,6 +71,48 @@ public abstract class UcsFunction implements MachineDependentFunction {
         case PARAMETERS -> UcsSequencer.setParameters(data);
         case ADMIN_STATUS -> UcsSequencer.setAdminStatus(data);
         default -> throw new IllegalStateException("not a UCS function: " + function);
+        }
+    }
+
+    /** 0x70 0x10, the waves */
+    public static class SharpWave extends UcsFunction {
+        public SharpWave() {
+            super(VENDOR_SHARP, WAVE);
+        }
+    }
+
+    /** 0x70 0x11, the voice parameters */
+    public static class SharpParameters extends UcsFunction {
+        public SharpParameters() {
+            super(VENDOR_SHARP, PARAMETERS);
+        }
+    }
+
+    /** 0x70 0x12, the tone a wave is */
+    public static class SharpAdminStatus extends UcsFunction {
+        public SharpAdminStatus() {
+            super(VENDOR_SHARP, ADMIN_STATUS);
+        }
+    }
+
+    /** 0x40 0x10, the waves */
+    public static class PanasonicWave extends UcsFunction {
+        public PanasonicWave() {
+            super(VENDOR_PANASONIC, WAVE);
+        }
+    }
+
+    /** 0x40 0x11, the voice parameters */
+    public static class PanasonicParameters extends UcsFunction {
+        public PanasonicParameters() {
+            super(VENDOR_PANASONIC, PARAMETERS);
+        }
+    }
+
+    /** 0x40 0x12, the tone a wave is */
+    public static class PanasonicAdminStatus extends UcsFunction {
+        public PanasonicAdminStatus() {
+            super(VENDOR_PANASONIC, ADMIN_STATUS);
         }
     }
 }
